7a24e53dc5ca64263f3998e31d61b04817ef31b8
unknown <husnimudzakir6@gmail.com>
2023-05-14 17:07:35 +0700

    Update

--- conncet.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.ns0bhrz.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
-
-doc = {
-    'test': 'test1'
-}
-db.user01.insert_one(doc)
